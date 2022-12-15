59081de6280e7d49e206ce9d4d5728869e17e8d2
thoshiai <45982834+thoshiai@users.noreply.github.com>
2022-12-15 19:03:49 +0900

    Fix segmentation fault in incorrect view def (#39)
    A segfault was occurred when non-SELECT query was specified in create_immv since the statement type was not checked.
    
    issue #37

--- pg_ivm.c
+++ pg_ivm.c
@@ -197,6 +197,12 @@ create_immv(PG_FUNCTION_ARGS)
 
 	parsetree = linitial_node(RawStmt, parsetree_list);
 
+	/* view definition should spcify SELECT query */
+	if (!IsA(parsetree->stmt, SelectStmt))
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				 errmsg("view definition must specify SELECT statement")));
+
 	ctas = makeNode(CreateTableAsStmt);
 	ctas->query = parsetree->stmt;
 #if defined(PG_VERSION_NUM) && (PG_VERSION_NUM >= 140000)
